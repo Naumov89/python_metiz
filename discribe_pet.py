0cb54f3760988491f64457fd2425bcd987c6e917
Oleksandr Naumov <s.naumov@ukr.net>
2021-03-26 21:59:09 +0200

    Study 2.1

---
+++ discribe_pet.py
@@ -0,0 +1,7 @@
+
+def discribe_pet(animal_type, pet_name):
+    print(f"\nI have {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+discribe_pet('hamster', 'harry')
+discribe_pet('dog', 'willie')
